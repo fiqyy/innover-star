20858bb9e773d60f6530e8c744f79affe907278e
fiqyy <fiqyyyy@gmail.com>
2026-01-30 07:09:03 +0200

    Created The Web App

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+//     <div className="App">
+//       <div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+//         <GradientBlinds
+//           gradientColors={["#0433ff","#5227FF"]}
+//           angle={130}
+//           noise={0.3}
+//           blindCount={16}
+//           blindMinWidth={60}
+//           mouseDampening={0.15}
+//           mirrorGradient={false}
+//           spotlightRadius={0.5}
+//           spotlightSoftness={1}
+//           spotlightOpacity={1}
+//           distortAmount={0}
+//           shineDirection="left"
+//         />
+// </div>
+    // </div>
+    <h1>hi</h1>
+  );
+}
+
+export default App;
