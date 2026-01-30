ef1d43ad82a45cc73a183ba2cf565870e347f14a
fiqyy <fiqyyyy@gmail.com>
2026-01-30 07:08:44 +0200

    Create Web App

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header () {
+    return (
+        <div className="header-com">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
